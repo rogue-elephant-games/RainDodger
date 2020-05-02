ef2b3f09548828455e3a4825874075606041dcf7
Rogue Elephant <jup.trandell@gmail.com>
2020-05-02 11:19:41 +0100

    Added enemy waves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,47 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 0.3f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.3f;
+
+    Coroutine firingCoroutine;
 
     float xMin, xMax, yMin, yMax;
 
-    // Start is called before the first frame update
     void Start()
     {
         SetUpMoveBoundaries();
+    }
+
+    void Update()
+    {
+        Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireContinuosly());
+        }
+        if(Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuosly()
+    {
+        while(true)
+        {
+            GameObject laser =
+                Instantiate(laserPrefab, transform.position, Quaternion.identity)
+                as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        }
     }
 
     private void SetUpMoveBoundaries()
@@ -22,12 +56,6 @@ public class Player : MonoBehaviour
         var min = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0));
         var max = gameCamera.ViewportToWorldPoint(new Vector3(1,1,0));
         (xMin, xMax, yMin, yMax) = (min.x +padding, max.x -padding, min.y + padding, max.y - padding);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
     }
 
     private void Move()
---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+   [SerializeField] GameObject enemyPrefab;
+   [SerializeField] GameObject pathPrefab;
+   [SerializeField] float timeBetweenSpawns = 0.5f;
+   [SerializeField] float spawnRandomFactor = 0.3f;
+   [SerializeField] int numberOfEnemies = 5;
+   [SerializeField] float moveSpeed = 2f;
+
+   public GameObject GetEnemyPrefab() => enemyPrefab;
+   public List<Transform> GetWaypoints() => 
+    pathPrefab.transform.Cast<Transform>().Select((Transform child) => child).ToList();
+   public float GetTimeBetweenSpawns() => timeBetweenSpawns;
+   public float GetSpawnRandomFactor() => spawnRandomFactor;
+   public float GetNumberOfEnemies() => numberOfEnemies;
+   public float GetMoveSpeed() => moveSpeed;
+}
