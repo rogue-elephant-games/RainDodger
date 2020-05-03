71725034b4acab00cb8b818a2b481d33cdc1bcce
Rogue Elephant <jup.trandell@gmail.com>
2020-05-03 07:04:38 +0100

    Added player laser dealing damage to enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        DamageDealer damageDealer = collider.gameObject.GetComponent<DamageDealer>();
+        if (damageDealer != null)
+            TakeDamage(damageDealer);
+    }
+
+    private void TakeDamage(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
